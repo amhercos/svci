79b1df5d0c9b7097e2be98725872969cf013f017
amhercos <148378926+amhercos@users.noreply.github.com>
2025-04-07 16:09:16 +0000

    $git commit -main $git push origin main $git commit -m

--- BAS/teacherdashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['role'] !== 'teacher') {
-    header('Location: login.php');
-    exit;
-}
-echo "<h1>Welcome, Teacher " . $_SESSION['username'] . "!</h1>";
-echo "<a href='logout.php'>Logout</a>";
